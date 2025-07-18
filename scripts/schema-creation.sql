9127046bffb848bc4552086aaba8d2bfc8bf57b6
Fırat Ergül <5615178+firate@users.noreply.github.com>
2025-07-18 16:59:23 +0300

    creation script fix

--- scripts/schema-creation.sql
+++ scripts/schema-creation.sql
@@ -1,7 +1,7 @@
 
 --  unique_id => unique like id column
 --  correlation_id => for distributed log tracing
- 
+--  Level: 0=Verbose, 1=Debug, 2=Information, 3=Warning, 4=Error, 5=Fatal
 
 CREATE TABLE IF NOT EXISTS logs (
     id INTEGER PRIMARY KEY,
@@ -9,36 +9,37 @@ CREATE TABLE IF NOT EXISTS logs (
     correlation_id TEXT,
     timestamp INTEGER NOT NULL,
     offset_minutes INTEGER NOT NULL,
-    level TEXT NOT NULL,
+    level INTEGER NOT NULL,  
     message TEXT NOT NULL,
     source TEXT NOT NULL
 );
 
+-- Unique_Id is real key
+
+CREATE UNIQUE INDEX IF NOT EXISTS idx_logs_unique_id ON logs(unique_id);
 CREATE INDEX IF NOT EXISTS idx_logs_timestamp ON logs(timestamp);
-CREATE INDEX IF NOT EXISTS idx_logs_level ON logs(level);
 CREATE INDEX IF NOT EXISTS idx_logs_timestamp_level ON logs(timestamp, level);
 
+-- FTS5 for message search
 CREATE VIRTUAL TABLE IF NOT EXISTS logs_fts USING fts5(
     message,
     content='logs',
     content_rowid='id'
 );
 
--- INSERT trigger
+-- FTS5 triggers
 CREATE TRIGGER IF NOT EXISTS logs_ai 
 AFTER INSERT ON logs
 BEGIN
 INSERT INTO logs_fts(rowid, message) VALUES (new.id, new.message);
 END;
 
--- UPDATE trigger
 CREATE TRIGGER IF NOT EXISTS logs_au 
 AFTER UPDATE ON logs
 BEGIN
 UPDATE logs_fts SET message = new.message WHERE rowid = new.id;
 END;
 
--- DELETE trigger
 CREATE TRIGGER IF NOT EXISTS logs_ad 
 AFTER DELETE ON logs
 BEGIN
